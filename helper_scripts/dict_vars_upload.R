c83924a73bdd8549f91fe082f3163dae3a9129b0
Sean Connelly <sconnelly005@gmail.com>
2021-08-18 11:25:39 -0500

    update helper script

--- helper_scripts/dict_vars_upload.R
+++ helper_scripts/dict_vars_upload.R
@@ -1,11 +1,27 @@
 
+# Libraries
+library(tidyverse);library(here)
+library(mongolite)
+
+# Load API keys and database connection information
+source(here("../functions.R"), local = TRUE)
+
 # Load dictionary
-dict_vars <- read_csv(here("dictionaries", "dict_vars.csv"))
+dict_vars <- read_csv(here("../dictionaries", "dict_vars.csv"))
 
 dict_vars <- dict_vars %>% 
   mutate("national_summary_text" = ifelse(is.na(national_summary_text),
                                           "",
                                           national_summary_text))
+
+# Check for summary text characters
+# spec_char_check <- dict_vars %>% 
+#   filter(national_summary_text != "") %>% 
+#   mutate("row_number" = row_number()) %>% 
+#   select( # row_number,var_readable, 
+#          national_summary_text)
+# 
+# spec_char_check
 
 # Write to database
 mongo_conn <- fun_mongo_connect(host_name = "host_dev",
